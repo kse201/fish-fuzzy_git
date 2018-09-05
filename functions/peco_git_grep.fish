3bb6a8f83c671a73c8ba48b0e13526d90d4c7892
kse201 <kse.201@gmail.com>
2018-09-05 12:05:10 +0900

    feature: add peco_git_grep

---
+++ functions/peco_git_grep.fish
@@ -0,0 +1,18 @@
+function peco_git_grep
+    set -l query $argv
+
+    git grep --line-number "$query" | peco --prompt "GIT GREP>" --query $query | head -1 | read select_line
+
+    if [ $select_line ]
+        set select_file (echo $select_line | awk -F':' '{print $1}')
+        set select_number (echo $select_line | awk -F':' '{print $2}')
+
+    switch $EDITOR
+        case nvim vim vi
+                eval "$EDITOR +$select_number $select_file"
+        case '*'
+                eval "$EDITOR $select_file"
+        end
+    end
+end
+
